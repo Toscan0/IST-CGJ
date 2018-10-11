2fa6880ddddadd054738d71a5fe2560e509bd5c2
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2018-10-11 14:58:32 +0100

    random implemented

---
+++ cgj-project/Allprojects/Project1/random.cpp
@@ -0,0 +1,15 @@
+#include "random.h"
+
+#include <time.h>       /* time */
+#include <stdlib.h>     /* srand, rand */
+
+
+int makeRandom() {
+	int a;
+
+	srand(time(NULL));
+
+	a = rand() % 10;
+	
+	return a;
+}
---
+++ cgj-project/Allprojects/Project1/random.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <time.h>       /* time */
+#include <stdlib.h>     /* srand, rand */
+
+int makeRandom(); // betewen 0-9
